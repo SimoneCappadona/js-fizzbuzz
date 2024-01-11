daf7cd05e53037159121790405dbe092aa80d705
SimoneCappadona <simocappa95@gmail.com>
2024-01-11 18:08:32 +0100

    add color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-const userCardFizzbuzz = document.getElementById('card');
-const userFizz = document.getElementById('fizz');
-const userBuzz= document.getElementById('buzz');
-const userFizzBuzz= document.getElementById('fizzbuzz');
+const userCardFizzbuzz = document.getElementById("row");
 // for (let i = 1; i <=100; i++){
 //     if(i % 15 == 0){
 //         console.log('FizzBuzz')
@@ -16,32 +13,27 @@ const userFizzBuzz= document.getElementById('fizzbuzz');
 //        console.log(i)
 //     }
 // }
-for (let i = 1; i <=100; i++){
-    let titleClass = "";
-    let textBox = "";
-    if(i % 15 == 0){
-        titleClass = "text-primary";
-        textBox = "fizzbuzz"
-    }
-    else if(i % 5 == 0){
-        titleClass = "text-warning";
-        textBox = "buzz"
-    }
-    else if(i % 3 == 0){
-        titleClass = "text-danger";
-        textBox = "fizz"
-    }
-    else{
-        textBox = i;
-    }  
-    const boxElement = document.createElement('div');
-    boxElement.classList.add('box')
-    boxElement.innerHTML += `
-      <div class="card">  
-        <div class="card-body">
-            <h3 class="${textBox , titleClass}">${textBox}</h3>
-          </div>  
-        </div>   
+
+for (let i = 1; i <= 100; i++) {
+  let titleClass = "";
+  let textBox = "";
+  if (i % 15 == 0) {
+    titleClass = "text-primary";
+    textBox = "FizzBuzz";
+  } else if (i % 5 == 0) {
+    titleClass = "text-warning";
+    textBox = "Buzz";
+  } else if (i % 3 == 0) {
+    titleClass = "text-danger";
+    textBox = "Fizz";
+  } else {
+    textBox = i;
+  }
+  userCardFizzbuzz.innerHTML += `
+     <div class="col-2 g-2">
+        <div class="card text-center my-color">
+            <h3 class="${textBox} ${titleClass}">${textBox}</h3>
+            </div>
+          </div>   
     `;
-    userCardFizzbuzz.append(boxElement); 
 }
